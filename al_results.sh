a532713adaf8e4d95182835e6bb7e1a432e28464
Garrett Wilson <g@floft.net>
2019-01-18 10:40:46 -0800

    Direct comparison for AL vs. DAL results
    Now average over all 84 runs (28 homes * 3 folds/home) rather than an
    average of the averages

--- al_results.sh
+++ al_results.sh
@@ -7,7 +7,13 @@
 #
 # Get averages, average each of the set of 4 numbers
 #
-grep -Eo 'Avg: [0-9\.]+' al_results.txt | sed 's/Avg: //' | awk '
+all="$(grep -Eo '\[[0-9\.\ ]+\]' al_results.txt | sed -r 's/(\[|\])//g' | tr -s ' ')"
+fold0="$(cut -d' ' -f1 <<< "$all")"
+fold1="$(cut -d' ' -f2 <<< "$all")"
+fold2="$(cut -d' ' -f3 <<< "$all")"
+all_folds="$(echo "$fold0"; echo "$fold1"; echo "$fold2";)"
+
+awk '
 {
     i=(NR-1)%4
     if (i==0)      { sum1+=$1; sumsq1+=($1)^2; }
@@ -20,9 +26,9 @@ grep -Eo 'Avg: [0-9\.]+' al_results.txt | sed 's/Avg: //' | awk '
     std2=sqrt((sumsq2-sum2^2/n)/n)
     std3=sqrt((sumsq3-sum3^2/n)/n)
     std4=sqrt((sumsq4-sum4^2/n)/n)
-    print "Averages over " n " homes (and each home is an average of 3-fold CV)"
+    print "Averages over " n " runs (and each home is an average of 3-fold CV)"
     print "Train A \t Avg: " sum1/n "\t Std: " std1
     print "Test A  \t Avg: " sum2/n "\t Std: " std2
     print "Train B \t Avg: " sum3/n "\t Std: " std3
     print "Test B  \t Avg: " sum4/n "\t Std: " std4
-}'
+}' <<< "$all_folds"

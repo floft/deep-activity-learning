ff2133a1bdc1b9f7b4185925bd4401a671a6d814
Garrett Wilson <g@floft.net>
2019-04-05 17:29:44 -0700

    put back tf_domain_labels

--- load_data.py
+++ load_data.py
@@ -241,6 +241,10 @@ def one_hot(x, y, num_classes, index_one=False):
 
     return x, y
 
+def tf_domain_labels(label, batch_size, num_domains=2):
+    """ Generate one-hot encoded labels for which domain data is from (using TensorFlow) """
+    return tf.tile(tf.one_hot([label], depth=num_domains), [batch_size,1])
+
 def domain_labels(label, batch_size, num_domains=2):
     """ Generate one-hot encoded labels for which domain data is from (using numpy) """
     return np.tile(np.eye(num_domains)[label], [batch_size,1])
